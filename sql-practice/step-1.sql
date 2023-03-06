c3db650f930b65b972fe5040936facc3acfb8ee4
Dmytro2V <op2vin@gmail.com>
2023-03-06 18:37:58 +0200

    done

--- sql-practice/step-1.sql
+++ sql-practice/step-1.sql
@@ -3,3 +3,9 @@
 -- join table on to one side of the relationship, then JOINing the other side
 -- on to the join table.
 -- Your code here
+.read seed-data.sql
+SELECT musicians.first_name, instruments.type 
+    FROM 
+        musician_instruments JOIN musicians ON (musician_instruments.musician_id = musicians.id)
+        JOIN  instruments ON (musician_instruments.instrument_id = instruments.id)
+;
--- sql-practice/step-2.sql
+++ sql-practice/step-2.sql
@@ -2,3 +2,10 @@
 -- Just like with one-to-many relationships, you can filter one table by any
 -- associated data on a conected table.
 -- Your code here
+.read seed-data.sql
+SELECT musicians.first_name, last_name, instruments.type 
+    FROM 
+        musician_instruments JOIN musicians ON (musician_instruments.musician_id = musicians.id)
+        JOIN  instruments ON (musician_instruments.instrument_id = instruments.id)
+    WHERE type = 'piano'
+;
